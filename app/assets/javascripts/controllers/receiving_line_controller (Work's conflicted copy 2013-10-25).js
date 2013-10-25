f62979f916282fb9470421921ef5269dea0f673d
John Ratcliff <john.w.ratcliff@biola.edu>
2013-10-25 11:39:06 -0700

    Removed conflict files

--- app/assets/javascripts/controllers/receiving_line_controller (Work's conflicted copy 2013-10-25).js
+++
@@ -1,9 +0,0 @@
-App.ReceivingLineController = Ember.ObjectController.extend({
-  needs: 'line_item',
-
-  highlightLineItem: function() {
-    console.log('yay');
-    console.log(this.get('line_item'));
-    console.log('yay');
-  }
-});
