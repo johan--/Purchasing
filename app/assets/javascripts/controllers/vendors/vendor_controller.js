02d537b6bd4ab660f4ae4628e5c9c26399e7f1d8
thec0keman <john.w.ratcliff@biola.edu>
2013-10-28 22:38:29 -0700

    Added vendor controllers back

---
+++ app/assets/javascripts/controllers/vendors/vendor_controller.js
@@ -0,0 +1,26 @@
+App.VendorController = Ember.ObjectController.extend({
+
+  hasEmail: function() {
+    var email = this.get('email');
+    return !Ember.isEmpty(email);
+  }.property('email'),
+
+  hasWebsite: function() {
+    var website = this.get('website');
+    return !Ember.isEmpty(website);
+  }.property('website'),
+
+  city_state: function() {
+    var city = this.get('city'),
+        state = this.get('state');
+    return (city) ? city + ', ' + state : state;
+  }.property('city', 'state'),
+
+  actions: {
+    openRecord: function() {
+      record = this.get('model');
+      this.send('openModal', 'VendorEdit', 'vendors/edit', record);
+      return false;
+    }
+  }
+})
