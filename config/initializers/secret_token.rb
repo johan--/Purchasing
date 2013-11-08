fdc7bc743e6cb39a8557cb45c3305fdc4843aa5f
John Ratcliff <john.w.ratcliff@biola.edu>
2013-11-08 10:09:20 -0800

    Small tweaks to test production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,6 @@ if Rails.env.development? || Rails.env.test?
   Purchasing::Application.config.secret_key_base = 'x' * 40
   puts 'Secret Token set to DEV mode'
 else
-  Purchasing::Application.config.secret_key_base = ENV['SECRET_TOKEN']
+  Purchasing::Application.config.secret_key_base = 'x' * 40 #ENV['SECRET_TOKEN']
   puts 'Secret Token set from ENV'
 end
