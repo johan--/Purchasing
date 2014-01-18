05a53c555a1c1dd68887d183037fb429c0b8498c
thec0keman <john.w.ratcliff@biola.edu>
2014-01-18 12:11:04 -0800

    Removed binding between search box and queryParams

--- app/assets/javascripts/views/search/search_field_view.js
+++ app/assets/javascripts/views/search/search_field_view.js
@@ -4,6 +4,12 @@ App.SearchFieldView = Ember.TextField.extend({
   placeholder: 'Search',
   classNames: ['form-control', 'search_box_input'],
 
-  action: 'startSearch'
+  action: 'startSearch',
+
+  // Don't bind queryParams directly to input, otherwise route will refresh on every character typed
+  didInsertElement: function() {
+    var val = this.get('parentView.value');
+    this.$().val(val);
+  }
 
 });
