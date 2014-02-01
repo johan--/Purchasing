a7faca818a5e382453f86ebb248ee25305998f1c
thec0keman <john.w.ratcliff@biola.edu>
2014-02-01 15:52:31 -0800

    Minor tweaks

--- app/assets/javascripts/mixins/metaData_mixin.js
+++ app/assets/javascripts/mixins/metaData_mixin.js
@@ -4,9 +4,8 @@ App.MetaDataMixin = Ember.Mixin.create({
 
   metadata: function() {
     if (this.get('model.isLoaded')) {
-      var store = this.store;
       var modelType = this.get('model.type');
-      var metadata = store.typeMapFor(modelType).metadata;
+      var metadata = this.store.typeMapFor(modelType).metadata;
 
       return metadata;
     }
