bcea202bd84c50af4d361dbac83740a4a3c0c8ff
thec0keman <john.w.ratcliff@biola.edu>
2014-02-11 21:28:29 -0800

    Fixed bug where purchase.new was calling broken controller

---
+++ app/assets/javascripts/controllers/purchase/purchase_attachments_new_controller.js
@@ -0,0 +1,7 @@
+
+App.PurchaseAttachmentsNewController = Ember.ArrayController.extend(App.AttachmentsControllerMixin, {
+  needs: ['application'],
+  applicationBinding: 'controllers.application',
+  itemController: 'attachment',
+
+});
