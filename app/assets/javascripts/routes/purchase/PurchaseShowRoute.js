7047c024ba4ea01244b71c574fcb35b65bf2be9a
thec0keman <john.w.ratcliff@biola.edu>
2014-02-12 21:45:50 -0800

    Reverted change to purchase.show

--- app/assets/javascripts/routes/purchase/PurchaseShowRoute.js
+++ app/assets/javascripts/routes/purchase/PurchaseShowRoute.js
@@ -20,6 +20,15 @@ App.PurchaseShowRoute = Ember.Route.extend(App.PurchaseRouteMixin, {
   },
 
 
+  activate: function() {
+    var parent = this,
+        record = this.modelFor('purchase.show');
+
+    if (!isEmpty(record) && !isEmpty(record.id))
+      record.reload();
+  },
+
+
   actions: {
 
     willTransition: function(transition) {
