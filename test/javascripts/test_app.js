1c190fb12b9e35f7d6dc2de169bea537b2b39ad8
John Ratcliff <john.w.ratcliff@biola.edu>
2014-02-17 15:32:36 -0800

    Added test user setup

--- test/javascripts/test_app.js
+++ test/javascripts/test_app.js
@@ -9,3 +9,12 @@ DS.Model.reopen({
   can_create: true,
   can_delete: true
 });
+
+App.current_user.set('id', 55);
+App.current_user.set('username', 'johndoe');
+App.current_user.set('name', 'John Doe');
+App.current_user.set('email', 'myName@someplace.org');
+App.current_user.set('phone', '542-231-5151');
+App.current_user.set('department', 'department');
+App.current_user.set('roles', ['admin']);
+App.current_user.set('photo_url', null);
