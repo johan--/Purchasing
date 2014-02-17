754ecebbd064b92cfa56bae2152ac8400479f0b8
thec0keman <john.w.ratcliff@biola.edu>
2014-02-17 00:38:46 -0800

    Several additional changes so templates will precompile.  Removed unnecessary rails templates and merged impersonate buttons into ember templates

--- convert_templates.rb
+++ convert_templates.rb
@@ -1,11 +1,11 @@
 
 Dir["./app/assets/javascripts/templates/**/*.*"].each do |s|
-  filename = s.gsub(/^.*templates/,'').gsub(/\/_/, '/').gsub(/^\//, '')
+    filename = s.gsub(/^.*templates/,'').gsub(/\/_/, '/').gsub(/^\//, '')
 
-  #header = "%script{ type: 'text/x-handlebars', 'data-template-name' => '#{filename.split('.')[0]}' }"
-  data = File.open(s) { |f| f.readlines.join() }
+    #header = "%script{ type: 'text/x-handlebars', 'data-template-name' => '#{filename.split('.')[0]}' }"
+    data = File.open(s) { |f| f.readlines.join() }
 
-  new_filename = s.gsub('.html.haml', '.js.hbs.haml')
-  File.open(new_filename, 'w') {|f| f.write(data)}
-  File.delete(s)
+    new_filename = s.gsub('/_', '/').gsub('.html.haml', '.js.hbs.hamlbars')
+    File.open(new_filename, 'w') {|f| f.write(data)}
+    File.delete(s)
 end
